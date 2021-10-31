5d374ea9d0884db8bd94ff65bd05de81cf752814
musai7 <musaishankar307@gmail.com>
2021-10-31 10:50:27 +0530

    added snake ladder for random number logic

---
+++ src/com/game/laddersnake/SnakeLadder_uc2.java
@@ -0,0 +1,10 @@
+package com.game.laddersnake;
+
+public class SnakeLadder_uc2 {
+	public static void main(String[] args) {
+		int player1Position = 0;
+			int diceNum = (int) (Math.random() * 6 + 1);
+			System.out.println("player one position " + player1Position);
+			System.out.println("Dice number : " + diceNum);
+	}
+}
